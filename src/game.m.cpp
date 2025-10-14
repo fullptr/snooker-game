9df7e4bcc70eea04d492fefd7baa7bc03aff9551
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-10-15 00:51:52 +0100

    Table Basics (#2)
    * Added a simple rectangular table, with no pockets.
    * Added a cue ball and 15 pool balls.
    * Implemented elastic collisions against the sides of the table.
    * Implemented slightly inelastic collisions between individual balls.
    * The collision logic is very simple for now and will get more complex in the future.
    * Currently, clicking will shoot the cue ball towards the cursor.

--- src/game.m.cpp
+++ src/game.m.cpp
@@ -19,6 +19,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+using namespace snooker;
+
 enum class next_state
 {
     main_menu,
@@ -27,11 +29,14 @@ enum class next_state
 };
 
 constexpr auto clear_colour = snooker::from_hex(0x222f3e);
+constexpr auto ball_radius = 2.54f; // english pool bool cm == 1 inch
+constexpr auto ball_mass = 140.0f; // grams
+constexpr auto board_colour = from_hex(0x3db81e);
+constexpr auto break_speed = 983.49f; // cm
 
-auto scene_main_menu(snooker::window& window) -> next_state
+auto scene_main_menu(snooker::window& window, snooker::renderer& renderer) -> next_state
 {
     auto timer = snooker::timer{};
-    auto renderer = snooker::renderer{};
     auto ui    = snooker::ui_engine{&renderer};
 
     while (window.is_running()) {
@@ -88,20 +93,167 @@ auto scene_main_menu(snooker::window& window) -> next_state
     return next_state::exit;
 }
 
-auto scene_game(snooker::window& window) -> next_state
+// dimensions are an english pool table in cm (6ft x 3ft)
+struct table
+{
+    f32 length;
+    f32 width;
+
+    auto dimensions() -> glm::vec2 { return {length, width}; }
+};
+
+struct ball
+{
+    glm::vec2 pos;
+    glm::vec2 vel;
+    glm::vec4 colour;
+
+    float mass = ball_mass;
+};
+
+// TODO: handle balls intersecting
+auto update_ball(ball& b, const table& t, float dt) -> void
+{
+    b.pos += b.vel * dt;
+    b.vel *= 0.98f;
+
+    if (b.pos.x - ball_radius < 0) {
+        b.pos.x = ball_radius;
+        b.vel.x = -b.vel.x;
+    }
+    if (b.pos.x + ball_radius > t.length) {
+        b.pos.x = t.length - ball_radius;
+        b.vel.x = -b.vel.x;
+    }
+
+    if (b.pos.y - ball_radius < 0) {
+        b.pos.y = ball_radius;
+        b.vel.y = -b.vel.y;
+    }
+    if (b.pos.y + ball_radius > t.width) {
+        b.pos.y = t.width - ball_radius;
+        b.vel.y = -b.vel.y;
+    }
+}
+
+// TODO: allow for balls of different masses
+auto update_ball_collision(ball& a, ball& b, const table& t, float dt) -> void
+{
+    if (glm::length(a.pos - b.pos) > 2 * ball_radius) {
+        return; // no contact
+    }
+
+    constexpr auto restitution = 0.8f;
+
+    const auto dp = a.pos - b.pos;
+    const auto dv = a.vel - b.vel;
+
+    if (glm::length2(dp) == 0) {
+        return;
+    }
+
+    const auto length2 = glm::dot(dp, dp);
+    if (length2 == 0.0f) {
+        return; // avoid division by zero
+    }
+
+    const auto normal = glm::normalize(dp);
+    const auto vel_along_normal = glm::dot(dv, normal);
+
+    if (vel_along_normal >= 0.0f) {
+        return; // moving away so no need to resolve
+    }
+
+    const auto inverse_mass = (1.0f / a.mass) + (1.0f / b.mass);
+    const auto impulse_size = -(1.0f + restitution) * vel_along_normal / inverse_mass;
+    const auto impulse = impulse_size * normal;
+
+    a.vel += impulse / a.mass;
+    b.vel -= impulse / b.mass;
+}
+
+auto add_triangle(std::vector<ball>& balls, glm::vec2 front_pos)
+{
+    const auto left = glm::vec2{std::sqrt(3) * ball_radius, -ball_radius};
+    const auto down = glm::vec2{0, 2 * ball_radius};
+
+    const auto red = glm::vec4{1, 0, 0, 1};
+    const auto yel = glm::vec4{1, 1, 0, 1};
+    const auto blk = glm::vec4{0, 0, 0, 1};
+
+    balls.push_back(ball{ front_pos + 0.0f * left + 0.0f * down, {0.0f, 0.0f}, red });
+
+    balls.push_back(ball{ front_pos + 1.0f * left + 0.0f * down, {0.0f, 0.0f}, red });
+    balls.push_back(ball{ front_pos + 1.0f * left + 1.0f * down, {0.0f, 0.0f}, yel });
+
+    balls.push_back(ball{ front_pos + 2.0f * left + 0.0f * down, {0.0f, 0.0f}, yel });
+    balls.push_back(ball{ front_pos + 2.0f * left + 1.0f * down, {0.0f, 0.0f}, blk });
+    balls.push_back(ball{ front_pos + 2.0f * left + 2.0f * down, {0.0f, 0.0f}, red });
+
+    balls.push_back(ball{ front_pos + 3.0f * left + 0.0f * down, {0.0f, 0.0f}, red });
+    balls.push_back(ball{ front_pos + 3.0f * left + 1.0f * down, {0.0f, 0.0f}, yel });
+    balls.push_back(ball{ front_pos + 3.0f * left + 2.0f * down, {0.0f, 0.0f}, red });
+    balls.push_back(ball{ front_pos + 3.0f * left + 3.0f * down, {0.0f, 0.0f}, yel });
+
+    balls.push_back(ball{ front_pos + 4.0f * left + 0.0f * down, {0.0f, 0.0f}, yel });
+    balls.push_back(ball{ front_pos + 4.0f * left + 1.0f * down, {0.0f, 0.0f}, yel });
+    balls.push_back(ball{ front_pos + 4.0f * left + 2.0f * down, {0.0f, 0.0f}, red });
+    balls.push_back(ball{ front_pos + 4.0f * left + 3.0f * down, {0.0f, 0.0f}, yel });
+    balls.push_back(ball{ front_pos + 4.0f * left + 4.0f * down, {0.0f, 0.0f}, red });
+}
+
+auto scene_game(snooker::window& window, snooker::renderer& renderer) -> next_state
 {
     using namespace snooker;
     auto timer    = snooker::timer{};
-    auto renderer = snooker::renderer{};
     auto ui       = snooker::ui_engine{&renderer};
 
+    auto pool_table = table{182.88f, 91.44f}; // english pool table dimensions in cm (6ft x 3ft)
+    auto pool_balls = std::vector{
+        ball{{50.0f, pool_table.width / 2.0f}, {0.0f, 0.0f}},
+    };
+    add_triangle(pool_balls, {0.8f * pool_table.length, pool_table.width / 2.0f});
+    
     while (window.is_running()) {
         const double dt = timer.on_update();
         window.begin_frame(clear_colour);
-
+        
+        auto& cue_ball = pool_balls[0];
+        const auto board_to_screen = (0.9f * window.width()) / pool_table.length;
+        const auto top_left = window.dimensions() / 2.0f - pool_table.dimensions() * board_to_screen / 2.0f;
+        const auto aim_direction = -glm::normalize(top_left + cue_ball.pos * board_to_screen - glm::vec2{window.mouse_pos()});
+        
         for (const auto event : window.events()) {
             ui.on_event(event);
+            if (const auto e = event.get_if<mouse_pressed_event>()) {
+                cue_ball.vel = 200.0f * aim_direction;
+            }
         }
+
+        // TODO: Fix the time step of the simulation with an accumulator
+
+        // Update ball positions
+        for (std::size_t i = 0; i != pool_balls.size(); ++i) {
+            for (std::size_t j = i + 1; j != pool_balls.size(); ++j) {
+                update_ball_collision(pool_balls[i], pool_balls[j], pool_table, (float)dt);
+            }
+        }
+
+        for (auto& ball : pool_balls) {
+            update_ball(ball, pool_table, (float)dt);
+        }
+
+        // Draw table
+        renderer.push_quad({window.width() / 2, window.height() / 2}, pool_table.length * board_to_screen, pool_table.width * board_to_screen, 0, board_colour);
+
+        // Draw balls
+        for (const auto& ball : pool_balls) {
+            renderer.push_circle(top_left + ball.pos * board_to_screen, ball.colour, ball_radius * board_to_screen);
+        }
+        renderer.push_circle(top_left + cue_ball.pos * board_to_screen, {1, 1, 1, 1}, ball_radius * board_to_screen);
+
+        // Draw cue
+        renderer.push_line(top_left + cue_ball.pos * board_to_screen, top_left + cue_ball.pos * board_to_screen + aim_direction * 5.0f * board_to_screen, {0, 0, 1, 1}, 2.0f);
 
         if (ui.button("Back", {0, 0}, 200, 50, 3)) {
             return next_state::main_menu;
@@ -119,16 +271,17 @@ auto main() -> int
 {
     using namespace snooker;
 
-    auto window = snooker::window{"The Way of snooker", 1280, 720};
+    auto window = snooker::window{"Snooker Game", 1280, 720};
+    auto renderer = snooker::renderer{};
     auto next   = next_state::main_menu;
 
     while (true) {
         switch (next) {
             case next_state::main_menu: {
-                next = scene_main_menu(window);
+                next = scene_main_menu(window, renderer);
             } break;
             case next_state::game: {
-                next = scene_game(window);
+                next = scene_game(window, renderer);
             } break;
             case next_state::exit: {
                 std::print("closing game\n");
