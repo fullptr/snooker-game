64a6cb282aeac9f2017448f86f3037aa3adb1d50
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2025-11-10 04:06:20 +0000

    Move the id_vector to a separate file

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp>
 
 #include "utility.hpp"
+#include "id_vector.hpp"
 
 namespace snooker {
 
@@ -31,60 +32,6 @@ struct collider
     float     mass; // non-positive mass == static
 };
 
-template <typename T>
-class id_vector
-{
-    std::vector<T>           d_data;
-    std::vector<std::size_t> d_data_id;
-    std::unordered_map<std::size_t, std::size_t> d_id_to_index;
-    std::size_t d_next = 0;
-
-public:
-    auto is_valid(std::size_t id) const -> bool
-    {
-        return d_id_to_index.contains(id);
-    }
-    auto add_collider(const T& c) -> std::size_t
-    {
-        const auto id = d_next++;
-        const auto index = d_data.size();
-        d_id_to_index[id] = index;
-        d_data.emplace_back(c);
-        d_data_id.emplace_back(id);
-        return id;
-    }
-    auto remove_collider(std::size_t id) -> void
-    {
-        assert_that(is_valid(id));
-        const auto index = d_id_to_index[id];
-
-        // swap the id to delete to the end
-        std::swap(d_data[index], d_data.back());
-        std::swap(d_data_id[index], d_data_id.back());
-
-        // update the map for the element swapped in
-        d_id_to_index[d_data_id[index]] = index;
-
-        // remove the element we want to remove
-        d_data.pop_back();
-        d_data_id.pop_back();
-    }
-    auto data() -> std::vector<T>&
-    {
-        return d_data;
-    }
-    auto get(std::size_t id) -> T&
-    {
-        assert_that(is_valid(id));
-        return d_data[d_id_to_index[id]];
-    }
-    auto get(std::size_t id) const -> const T&
-    {
-        assert_that(is_valid(id));
-        return d_data[d_id_to_index.at(id)];
-    }
-};
-
 class simulation
 {
     id_vector<collider> d_colliders;
@@ -93,7 +40,7 @@ public:
     auto add_circle(glm::vec2 pos, float radius, float mass) -> std::size_t
     {
         const auto col = collider{ .pos=pos, .vel=glm::vec2{0, 0}, .geometry=circle_shape{radius}, .mass=mass };
-        const auto id = d_colliders.add_collider(col);
+        const auto id = d_colliders.insert(col);
         return id;
     }
 
@@ -101,7 +48,7 @@ public:
     auto add_box(glm::vec2 centre, float width, float height) -> std::size_t
     {
         const auto col = collider{ .pos=centre, .vel=glm::vec2{0, 0}, .geometry=box_shape{.width=width, .height=height}, .mass=-1};
-        const auto id = d_colliders.add_collider(col);
+        const auto id = d_colliders.insert(col);
         return id;
     }
 
@@ -127,7 +74,7 @@ public:
     auto remove(std::size_t id) -> void
     {
         assert_that(is_valid(id));
-        d_colliders.remove_collider(id);
+        d_colliders.erase(id);
     }
 };
 
